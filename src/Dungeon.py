491021102bbab217bfcad78450a6c38c6c968c30
Pamela Touodop Mbeinda <pamelambienda@gmail.com>
2024-12-06 00:04:59 -0600

    New changes

---
+++ src/Dungeon.py
@@ -0,0 +1,14 @@
+dungeon = {"text": "You are in room 10! And you level is still at 55. What are you doing next?",
+                   "choices": {
+                       "There is a monster level 99": "room 13",
+                       "There is a spider monster level 23, you can kill it!": "room 20",
+                       "there is a huge pit there." : "room 16"
+                   },
+
+                   "room 13":{ "text": "You got killed and you lost" },
+
+                   "room 20": {"text": "After killing te monster, you get the treasure and win"},
+                   
+                   "room 16":{"text": "you will fall and die."}
+
+}
---
+++ src/Spaceship.py
@@ -0,0 +1,22 @@
+spaceshp = {
+    "text": "Welcome on board. Let's go on a adventure!!!!!!!",
+    "choices":{
+                 "A": "Let's go to new planet",
+                 "B": "Let's visit the space"},
+
+                 "Let's go to new planet":{
+                         "text": " you encounter a monster",
+                         "choices":{
+                                  "Your companion runs away": "You get killed and you lost",
+                                  "You killed the monster with a laser": "You won"}
+                 },
+
+                 "Let's visit the space":{
+                          "text":"You encounter troubles",
+                          "choices":{
+                                    "The spaceship broke down": " You lost",
+                                    "You jumped out and find a new one and treasure": "you win"
+                          }
+                     
+                 }
+}
